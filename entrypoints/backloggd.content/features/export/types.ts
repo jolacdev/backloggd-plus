7719b1097a60354e271ec2efefd5714ad3396567
Jose L <58451072+jolacdev@users.noreply.github.com>
2026-02-12 18:15:20 +0100

    feat(export): create specific types for the CSV export

---
+++ entrypoints/backloggd.content/features/export/types.ts
@@ -0,0 +1,35 @@
+/* eslint-disable perfectionist/sort-object-types */
+type GameDetailsCSV = {
+  // GET - Profile Games Page
+  id: string;
+  name: string;
+  backloggd_url: string; // https://backloggd.com + `path`
+
+  // GET - Game Log Details
+  // Attributes from `game_log`
+  // --- Status ---
+  is_play: boolean;
+  is_playing: boolean;
+  is_backlog: boolean;
+  is_wishlist: boolean;
+  // --- Flags ---
+  game_liked: boolean;
+  // Attributes from `playthroughs[0]`
+  // --- Time Played ---
+  hours_played: number;
+  mins_played: number;
+  hours_finished: number;
+  mins_finished: number;
+  hours_mastered: number;
+  mins_mastered: number;
+  // --- Dates ---
+  start_date: string;
+  finish_date: string;
+  // --- Flags ---
+  is_master: string;
+  is_replay: string;
+  // --- Review ---
+  rating: number;
+  review: string;
+  review_spoilers: boolean;
+};
